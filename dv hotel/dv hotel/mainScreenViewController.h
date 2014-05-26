b915b8738663285c57e91f9fccdd623e63120806
Dmytro Kikhtenko <shaffle.shaffle@gmail.com>
2014-05-26 23:34:35 +0400

    new storyboard new controllers

---
+++ dv hotel/dv hotel/mainScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  mainScreenViewController.h
+//  dv hotel
+//
+//  Created by Admin on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainScreenViewController : UIViewController
+
+@end
---
+++ dv hotel/dv hotel/mainScreenViewController.m
@@ -0,0 +1,49 @@
+//
+//  mainScreenViewController.m
+//  dv hotel
+//
+//  Created by Admin on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import "mainScreenViewController.h"
+
+@interface mainScreenViewController ()
+
+@end
+
+@implementation mainScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
